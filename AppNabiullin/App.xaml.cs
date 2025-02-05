28ac3513abc67f49dc191433ee4ace27eb94d220
pjqpdqjdp <sjakaj123@gmail.com>
2025-02-05 17:14:59 +0300

    начало

---
+++ AppNabiullin/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppNabiullin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppNabiullin/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppNabiullin
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
